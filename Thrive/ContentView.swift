ae0840b13815aef505903f135074594876ef404f
Vincent Cloutier <vcloutier@lcs.on.ca>
2021-10-04 16:42:22 -0400

    Initial Commit

---
+++ Thrive/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Thrive
+//
+//  Created by Vincent Cloutier on 2021-10-04.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Thrive/ThriveApp.swift
@@ -0,0 +1,17 @@
+//
+//  ThriveApp.swift
+//  Thrive
+//
+//  Created by Vincent Cloutier on 2021-10-04.
+//
+
+import SwiftUI
+
+@main
+struct ThriveApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
