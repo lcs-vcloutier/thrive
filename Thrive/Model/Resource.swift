6cc7d97c351d6d23af7d2ff3b13c7c342db0b982
Vincent Cloutier <vcloutier@lcs.on.ca>
2021-10-18 17:18:40 -0400

    gave each name a unique one

--- Thrive/Model/Resource.swift
+++ Thrive/Model/Resource.swift
@@ -19,10 +19,10 @@ struct Resource {
 extension Resource {
 
     static let staticData: [Resource] = [
-        Resource(name: "Runza", imageName: "person.fill", formattedphoneNumber: "tel:7059271903", phoneNumber: "(705) 927-1903", formattedEmail: "mailto:jrunza@lcs.on.ca", email: "jrunza@lcs.on.ca"),
-        Resource(name: "Runza", imageName: "person.fill", formattedphoneNumber: "tel:7059271903", phoneNumber: "(705) 927-1903", formattedEmail: "mailto:jrunza@lcs.on.ca", email: "jrunza@lcs.on.ca"),
-        Resource(name: "Runza", imageName: "person.fill", formattedphoneNumber: "tel:7059271903", phoneNumber: "(705) 927-1903", formattedEmail: "mailto:jrunza@lcs.on.ca", email: "jrunza@lcs.on.ca"),
-        Resource(name: "Runza", imageName: "person.fill", formattedphoneNumber: "tel:7059271903", phoneNumber: "(705) 927-1903", formattedEmail: "mailto:jrunza@lcs.on.ca", email: "jrunza@lcs.on.ca"),
-        Resource(name: "Runza", imageName: "person.fill", formattedphoneNumber: "tel:7059271903", phoneNumber: "(705) 927-1903", formattedEmail: "mailto:jrunza@lcs.on.ca", email: "jrunza@lcs.on.ca")
+        Resource(name: "Runza 0", imageName: "person.fill", formattedphoneNumber: "tel:7059271903", phoneNumber: "(705) 927-1903", formattedEmail: "mailto:jrunza@lcs.on.ca", email: "jrunza@lcs.on.ca"),
+        Resource(name: "Runza 1", imageName: "person.fill", formattedphoneNumber: "tel:7059271903", phoneNumber: "(705) 927-1903", formattedEmail: "mailto:jrunza@lcs.on.ca", email: "jrunza@lcs.on.ca"),
+        Resource(name: "Runza 2", imageName: "person.fill", formattedphoneNumber: "tel:7059271903", phoneNumber: "(705) 927-1903", formattedEmail: "mailto:jrunza@lcs.on.ca", email: "jrunza@lcs.on.ca"),
+        Resource(name: "Runza 3", imageName: "person.fill", formattedphoneNumber: "tel:7059271903", phoneNumber: "(705) 927-1903", formattedEmail: "mailto:jrunza@lcs.on.ca", email: "jrunza@lcs.on.ca"),
+        Resource(name: "Runza 4", imageName: "person.fill", formattedphoneNumber: "tel:7059271903", phoneNumber: "(705) 927-1903", formattedEmail: "mailto:jrunza@lcs.on.ca", email: "jrunza@lcs.on.ca")
     ]
 }
