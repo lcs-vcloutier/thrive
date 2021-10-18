a066f6051502846047e1f0102a54e324c393ac10
Vincent Cloutier <vcloutier@lcs.on.ca>
2021-10-18 16:55:49 -0400

    moved notification code and modified content view

--- Thrive/Views/ContentView.swift
+++ Thrive/Views/ContentView.swift
@@ -10,54 +10,16 @@ import UserNotifications
 
 struct ContentView: View {
     var body: some View {
-        VStack{
-            // This will handle user notificaiton
-            // Tutorial at link: https://www.youtube.com/watch?v=ERy8RnYUKjI
-            // No idea how to make this process automatic
-            // IMPORTANT: the user had to click on this button for the notification to work
-            // The idea is to schedule a year worth of notification all at once. I think there is a better way of doing it, but I can't be bothered. I don't think anyone will be using this app for more than a year anyway.
-            Button("Request Permission") {
-                UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .badge, .sound]){ success, error in
-                    if(success){
-                        print("Setting up notification")
-                    }else{
-                        print("GG")
-                    }
-                    
-                }
-                
-            }
-            Button("Schedule Notification")
-            {
-                setUpNoification()
-            }
+        VStack {
             
-            Text("Hello, world!")
-            // Explain what the app's about
-            // define thrive/jack chapter
-            // shoutout developers
+            // TO DO: Explain what the app's about
+            // TO DO: Explain THRIVE
+            // TO DO: Explain Developers
+            Button("Would you like to recieve motivational quotes everyday?", action: setUpNotification)
+            
         }
-    }
-    
-    // This funciton will schedule a year worth of notificaiton.
-    // Note: the varaiable motivationalQuotes could be found in the file notification Handler!
-    func setUpNoification() {
-        
-        for i in 1...365{
-            let content = UNMutableNotificationContent()
-            content.title = "Motivational Quote of The Day"
-            content.subtitle = movtivationalQuotes[Int.random(in:0..<movtivationalQuotes.count)]
-            
-            
-            let trigger = UNTimeIntervalNotificationTrigger(timeInterval: TimeInterval(24 * 60 * i), repeats: false)
-            let request = UNNotificationRequest(identifier: UUID().uuidString, content: content, trigger: trigger)
-            UNUserNotificationCenter.current().add(request)
+        .task {
+            askUserNotificationPermission()
         }
-        
-    }
-}
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
     }
 }
