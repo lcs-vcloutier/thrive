6df6f5c6f6495ab23b2b4ee3f20fbbdfeab07b4e
Vincent He <vincent0710@Outlook.com>
2021-11-01 17:17:36 -0400

    Homepage added! My attempt at making a homepage.

--- Thrive/Views/ContentView.swift
+++ Thrive/Views/ContentView.swift
@@ -9,20 +9,92 @@ import SwiftUI
 import UserNotifications
 
 struct ContentView: View {
+    // Add user deafult to setup notification automaitcally
+    
     var body: some View {
-        NavigationView{
-            VStack {
+        VStack {
+            ScrollView{
                 // TO DO: Explain what the app's about
+                HStack{
+                    Text("About App")
+                        .font(.system(size: 32, weight: .semibold, design: .default))
+                    Spacer()
+                }
+                
+                Image("Thrive")
+                    .resizable()
+                    .scaledToFit()
+                
+                Text("Thrive At Lakefield Is Something Cool And Fun!")
+                    .font(.system(size: 16, weight: .light, design: .default))
+                    .multilineTextAlignment(.center)
+                
+                
+                VStack{
+                    // For alignment
+                    HStack{
+                        Text("Fun Facts")
+                            .font(.system(size: 32, weight: .semibold, design: .default))
+                        Spacer()
+                    }
+                        .padding()
+                    
+                    ScrollView(.horizontal, showsIndicators: true){
+                        HStack{
+                            ForEach(0..<10){ num in // Fun Facts
+                                VStack{
+                                    Image("FunFacts")
+                                        .resizable()
+                                        .frame(width: 150, height: 150)
+                                    Text("Fun Facts / Random Statistics")
+                                        .font(.system(size: 16, weight: .light, design: .default))
+                                        .multilineTextAlignment(.center)
+                                }
+                            }
+                        }
+                    }
+                    Spacer()
+                }
+                    
+                
+                
+                
                 // TO DO: Explain THRIVE
+                HStack{
+                    Text("Explain Thrive")
+                        .font(.system(size: 32, weight: .semibold, design: .default))
+                    Spacer()
+                }
+                
+                Image("Thrive")
+                    .resizable()
+                    .scaledToFit()
+                
+                Text("Thrive At Lakefield Is Something Cool And Fun!")
+                    .font(.system(size: 16, weight: .light, design: .default))
+                    .multilineTextAlignment(.center)
+                
+                
                 // TO DO: Explain Developers
-                
+                HStack{
+                    Text("Explain Developers")
+                        .font(.system(size: 32, weight: .semibold, design: .default))
+                }
+
+                
+                // Add link to mental health at Lakefieldd
+                
+                // this button is only for placeholder
                 Button("Would you like to receive motivational quotes everyday?", action: setUpNotification)
-                Text("Hello World")
+                
+                Spacer() // Pushes everything to the top
             }
-            .navigationBarTitle("Title / Placeholder")
+            
+            .navigationBarTitle("Home Page")
         }
         .task {
             askUserNotificationPermission()
         }
+        
     }
 }
